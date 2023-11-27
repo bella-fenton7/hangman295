f123f593155e3d3d6e8bee99b932d28496a7de54
bella-fenton7 <104842776+bella-fenton7@users.noreply.github.com>
2023-11-27 16:31:19 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -14,23 +14,23 @@ def check_guess(guess, secret_word):
 def ask_for_input():
     """Ask the user to guess a letter and validate the input."""
     while True:
-        # Step 2: Move the code to check if the input is a valid guess into this function block.
+        # Step 2: check if the input is a valid guess into this function block.
         guess = input("Guess a letter: ")
 
-        # Step 3: Check that the guess is a single alphabetical character.
+        # Step 3: Check guess is a single alphabetical character.
         if guess.isalpha() and len(guess) == 1:
             # Step 4: If the guess passes the checks, break out of the loop.
             print("You guessed:", guess)
             break
         else:
-            # If the guess does not pass the checks, print an error message.
+            # If the guess does not pass checks, print error message.
             print("Invalid letter. Please, enter a single alphabetical character.")
 
-    # Step 5: Outside the while loop, call the check_guess function to check if the guess is in the word.
+    # Step 5: Outside the while loop, call check_guess function to check if guess in word.
     check_guess(guess, secret_word)
 
-# Secret word (replace this with your logic to choose a random word)
+# Secret word
 secret_word = "apple"
 
-# Step 6: Outside the function, call the ask_for_input function to test your code.
+# Step 6: Outside  function, call the ask_for_input function to test code.
 ask_for_input()
